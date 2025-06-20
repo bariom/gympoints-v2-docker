6c8ba4e0e3b2c9d224243482e73f240ea9ea2076
BariOm <omar.bariffi@gmail.com>
2025-06-20 12:12:40 +0200

    Logo applicazione in admin

--- app_gara1/admin.py
+++ app_gara1/admin.py
@@ -17,10 +17,10 @@ from pdf_export import export_pdf_results
 
 
 # Utility immagine base64
+
 def image_to_base64(path):
     with open(path, "rb") as img_file:
-        b64_data = base64.b64encode(img_file.read()).decode("utf-8")
-    return f"data:image/png;base64,{b64_data}"
+        return base64.b64encode(img_file.read()).decode("utf-8")
 
 def mostra_logo(titolo):
     logo_path = os.path.join("img", "logo.png")
